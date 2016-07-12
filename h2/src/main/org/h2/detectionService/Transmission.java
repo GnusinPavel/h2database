8ab8ce21f5f7372117e548ddb79c50e471e27e35
micr0s <micr0s@mail.ru>
2016-07-12 10:54:16 +0300

    Перегрузил конструктор и добавил определение адресса

--- h2/src/main/org/h2/detectionService/Transmission.java
+++ h2/src/main/org/h2/detectionService/Transmission.java
@@ -10,36 +10,36 @@ import java.net.SocketException;
  * Created by Pavel Kulkov  on 11.07.2016.
  */
 public class Transmission extends Thread {
-        private DatagramSocket socket = null;
-        private DatagramPacket packet = null;
-        private final int PORT = 4445;
-        private String addressGroup = "10.6.116.255";
+    private DatagramSocket socket = null;
+    private DatagramPacket packet = null;
+    private final int PORT = 4445;
+    private String addressGroup = "10.6.116.255";
 
-        public Transmission(){
+    public Transmission() {
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
             try {
-                socket = new DatagramSocket();
-            } catch (SocketException e) {
+                byte[] buf = new byte[256];
+                String msg = "h2o" + InetAddress.getLocalHost().getHostAddress();
+                buf = msg.getBytes();
+                InetAddress group = InetAddress.getByName(addressGroup);
+                packet = new DatagramPacket(buf, buf.length, group, PORT);
+                socket.send(packet);
+                sleep(500);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
-        public void run(){
-            for(int i=0;i<10;i++){
-                try {
-                    byte[] buf = new byte[256];
-                    String msg = "h2o"+InetAddress.getLocalHost().getHostAddress();
-                    buf = msg.getBytes();
-                    InetAddress group = InetAddress.getByName(addressGroup);
-                    packet = new DatagramPacket(buf, buf.length, group, PORT);
-                    socket.send(packet);
-                    sleep(500);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            socket.close();
-        }
+        socket.close();
     }
+}
 
